9b5b3f2b04c5f628b62003bb2fe491504c7d4924
Gustavo Orelio <oreliogustavo@gmail.com>
2025-03-22 14:11:41 -0300

    feat: add Header component and banner

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,34 @@
-import { Button } from '@/app/_components/ui/button'
-
+import Header from './_components/header'
+import { Input } from './_components/ui/input'
+import { Button } from './_components/ui/button'
+import { SearchIcon } from 'lucide-react'
+import Image from 'next/image'
 const Home = () => {
-  return <Button>Click me</Button>
+  return (
+    <div>
+      <Header />
+      <div className="p-5">
+        <h2 className="text-xl font-bold">Olá, Gustavo!</h2>
+        <p className="text-sm text-muted-foreground">Sabado, 22 de Março.</p>
+
+        <div className="mt-6 flex items-center gap-2">
+          <Input placeholder="Faça sua busca..." />
+          <Button variant="outline">
+            <SearchIcon />
+          </Button>
+        </div>
+
+        <div className="relative mt-6 h-[150px] w-full">
+          <Image
+            src="/banner-01.png"
+            fill
+            alt="Agende nos melhores com FSW Barber"
+            className="rounded-xl object-cover"
+          />
+        </div>
+      </div>
+    </div>
+  )
 }
 
 export default Home
