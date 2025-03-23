8d7be6c08c2d06224b8205bd831a24fb8e3958d0
Gustavo Orelio <oreliogustavo@gmail.com>
2025-03-22 21:41:47 -0300

    feat: add SidebarSheet component

--- app/_components/header.tsx
+++ app/_components/header.tsx
@@ -1,11 +1,11 @@
-import { MenuIcon, HomeIcon, CalendarIcon, LogOutIcon } from 'lucide-react'
+import { MenuIcon } from 'lucide-react'
 import { Button } from './ui/button'
 import { Card, CardContent } from './ui/card'
 import Image from 'next/image'
-import { Sheet, SheetTrigger, SheetContent, SheetHeader, SheetTitle, SheetClose } from './ui/sheet'
-import { quickSearchOptions } from '../_constants/search'
-import { Avatar, AvatarImage } from './ui/avatar'
-import Link from 'next/link'
+import { Sheet, SheetTrigger } from './ui/sheet'
+
+import SidebarSheet from './sidebar-sheet'
+
 const Header = () => {
   return (
     <Card>
@@ -18,52 +18,7 @@ const Header = () => {
               <MenuIcon />
             </Button>
           </SheetTrigger>
-          <SheetContent className="overflow-y-auto">
-            <SheetHeader>
-              <SheetTitle className="text-start">Menu</SheetTitle>
-            </SheetHeader>
-
-            <div className="flex items-center gap-3 border-b border-solid py-5">
-              <Avatar>
-                <AvatarImage src="https://github.com/diego3g.png" />
-              </Avatar>
-              <div className="flex flex-col">
-                <p className="font-bold">Diego Fernandes</p>
-                <p className="text-xs">diego@rocketseat.com.br</p>
-              </div>
-            </div>
-
-            <div className="flex flex-col gap-2 border-b border-solid py-5">
-              <SheetClose asChild>
-                <Button variant="ghost" className="justify-start gap-2" asChild>
-                  <Link href="/">
-                    <HomeIcon size={18} />
-                    Início
-                  </Link>
-                </Button>
-              </SheetClose>
-              <Button variant="ghost" className="justify-start gap-2">
-                <CalendarIcon size={18} />
-                Agendamentos
-              </Button>
-            </div>
-
-            <div className="flex flex-col gap-2 border-b border-solid py-5">
-              {quickSearchOptions.map(option => (
-                <Button variant="ghost" className="justify-start gap-2" key={option.title}>
-                  <Image src={option.imageUrl} alt={option.title} width={18} height={18} />
-                  {option.title}
-                </Button>
-              ))}
-            </div>
-
-            <div className="flex flex-col gap-2 py-5">
-              <Button variant="ghost" className="justify-start gap-2">
-                <LogOutIcon size={18} />
-                Sair da conta
-              </Button>
-            </div>
-          </SheetContent>
+          <SidebarSheet />
         </Sheet>
       </CardContent>
     </Card>
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,8 +1,10 @@
-import ServiceItem from '@/app/_components/service-item'
-import { Button } from '@/app/_components/ui/button'
 import PhoneItem from '@/app/_components/phone-item'
+import ServiceItem from '@/app/_components/service-item'
+import SidebarSheet from '@/app/_components/sidebar-sheet'
+import { Button } from '@/app/_components/ui/button'
+import { Sheet, SheetTrigger } from '@/app/_components/ui/sheet'
 import { db } from '@/app/_lib/prisma'
-import { ChevronLeftIcon, MenuIcon, MapPinIcon, StarIcon } from 'lucide-react'
+import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import { notFound } from 'next/navigation'
@@ -31,30 +33,40 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
     <div>
       {/* IMAGEM */}
       <div className="relative h-[250px] w-full">
-        <Image src={barbershop?.imageUrl} alt={barbershop?.name} fill className="object-cover" />
+        <Image alt={barbershop.name} src={barbershop?.imageUrl} fill className="object-cover" />
+
         <Button size="icon" variant="secondary" className="absolute left-4 top-4" asChild>
           <Link href="/">
             <ChevronLeftIcon />
           </Link>
         </Button>
-        <Button size="icon" variant="secondary" className="absolute right-4 top-4" asChild>
+
+        <Button size="icon" variant="secondary" className="absolute right-4 top-4">
           <MenuIcon />
         </Button>
+        <Sheet>
+          <SheetTrigger asChild>
+            <Button size="icon" variant="outline" className="absolute right-4 top-4">
+              <MenuIcon />
+            </Button>
+          </SheetTrigger>
+          <SidebarSheet />
+        </Sheet>
       </div>
-      {/* INFORMAÇÕES */}
+      {/* TÍTULO */}
       <div className="border-b border-solid p-5">
-        <h1 className="mb-3 text-xl font-bold">{barbershop?.name}</h1>
+        <h1 className="mb-3 text-xl font-bold">{barbershop.name}</h1>
         <div className="mb-2 flex items-center gap-2">
-          <MapPinIcon size={18} className="text-primary" />
+          <MapPinIcon className="text-primary" size={18} />
           <p className="text-sm">{barbershop?.address}</p>
         </div>
         <div className="flex items-center gap-2">
-          <StarIcon size={18} className="fill-primary text-primary" />
-          <p className="text-sm">5,0 (100 avaliações)</p>
+          <StarIcon className="fill-primary text-primary" size={18} />
+          <p className="text-sm">5,0 (499 avaliações)</p>
         </div>
       </div>
       {/* DESCRIÇÃO */}
-      <div className="space-y-3 border-b border-solid p-5">
+      <div className="space-y-2 border-b border-solid p-5">
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop?.description}</p>
       </div>
