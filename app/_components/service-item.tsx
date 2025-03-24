68d417dd316fc0809ca652dd54704f3d6086ef92
Gustavo Orelio <oreliogustavo@gmail.com>
2025-03-24 19:54:34 -0300

    feat: ensure today times are not shown if they are on the past

--- app/_components/service-item.tsx
+++ app/_components/service-item.tsx
@@ -6,8 +6,8 @@ import { Card, CardContent } from './ui/card'
 import { Sheet, SheetContent, SheetFooter, SheetHeader, SheetTitle } from './ui/sheet'
 import { Calendar } from './ui/calendar'
 import { ptBR } from 'date-fns/locale/pt-BR'
-import { useState, useEffect } from 'react'
-import { format, set } from 'date-fns'
+import { useState, useEffect, useMemo } from 'react'
+import { format, isPast, isToday, set } from 'date-fns'
 import { Barbershop, BarbershopService, Booking } from '@prisma/client'
 import { createBooking } from '../_actions/create-booking'
 import { toast } from 'sonner'
@@ -45,10 +45,21 @@ const TIME_LIST = [
   '18:00',
 ]
 
-const getTimeList = (bookings: Booking[]) => {
+interface GetTimeListProps {
+  bookings: Booking[]
+  selectedDay: Date
+}
+
+const getTimeList = ({ bookings, selectedDay }: GetTimeListProps) => {
   return TIME_LIST.filter(time => {
     const hour = Number(time.split(':')[0])
     const minute = Number(time.split(':')[1])
+
+    const timeIsOnThePast = isPast(set(selectedDay, { hours: hour, minutes: minute }))
+
+    if (timeIsOnThePast && isToday(selectedDay)) {
+      return false
+    }
 
     const hasBookingOnCurrentTime = bookings.some(
       booking => booking.date.getHours() === hour && booking.date.getMinutes() === minute
@@ -124,6 +135,11 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
     }
   }
 
+  const timeList = useMemo(() => {
+    if (!selectedDay) return []
+    return getTimeList({ bookings: dayBookings, selectedDay })
+  }, [dayBookings, selectedDay])
+
   return (
     <>
       <Card>
@@ -194,16 +210,20 @@ const ServiceItem = ({ service, barbershop }: ServiceItemProps) => {
 
                   {selectedDay && (
                     <div className="flex gap-3 overflow-x-auto border-b border-solid p-5 [&::-webkit-scrollbar]:hidden">
-                      {getTimeList(dayBookings).map(time => (
-                        <Button
-                          key={time}
-                          variant={selectedTime === time ? 'default' : 'outline'}
-                          className="rounded-full"
-                          onClick={() => handleTimeSelect(time)}
-                        >
-                          {time}
-                        </Button>
-                      ))}
+                      {timeList.length > 0 ? (
+                        timeList.map(time => (
+                          <Button
+                            key={time}
+                            variant={selectedTime === time ? 'default' : 'outline'}
+                            className="rounded-full"
+                            onClick={() => handleTimeSelect(time)}
+                          >
+                            {time}
+                          </Button>
+                        ))
+                      ) : (
+                        <p className="text-xs">Não há horários disponíveis para esse dia</p>
+                      )}
                     </div>
                   )}
 
