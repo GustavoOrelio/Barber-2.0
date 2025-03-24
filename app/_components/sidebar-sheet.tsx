b637fae41f8ece4fe6f8ee2ec01854a30a864c3e
Gustavo Orelio <oreliogustavo@gmail.com>
2025-03-24 14:15:59 -0300

    feat: ensure only signed in user can book

--- app/_components/sidebar-sheet.tsx
+++ app/_components/sidebar-sheet.tsx
@@ -4,22 +4,16 @@ import { Button } from './ui/button'
 import { CalendarIcon, HomeIcon, LogOutIcon, LogInIcon } from 'lucide-react'
 import { SheetClose, SheetContent, SheetHeader, SheetTitle } from './ui/sheet'
 import { quickSearchOptions } from '../_constants/search'
-import {
-  Dialog,
-  DialogContent,
-  DialogDescription,
-  DialogHeader,
-  DialogTitle,
-  DialogTrigger,
-} from './ui/dialog'
+import { Dialog, DialogTrigger, DialogContent } from './ui/dialog'
 import Link from 'next/link'
 import Image from 'next/image'
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { signOut, useSession } from 'next-auth/react'
 import { Avatar, AvatarImage } from './ui/avatar'
+import SignInDialog from './sign-in-dialog'
 
 const SidebarSheet = () => {
   const { data } = useSession()
-  const handleLoginWithGoogleClick = () => signIn('google')
+
   const handleLogoutClick = () => signOut()
 
   return (
@@ -50,18 +44,7 @@ const SidebarSheet = () => {
                 </Button>
               </DialogTrigger>
               <DialogContent className="w-[90%]">
-                <DialogHeader>
-                  <DialogTitle>Faça login na plataforma</DialogTitle>
-                  <DialogDescription>Conecte-se usando sua conta do Google.</DialogDescription>
-                </DialogHeader>
-                <Button
-                  variant="outline"
-                  className="gap-1 font-bold"
-                  onClick={handleLoginWithGoogleClick}
-                >
-                  <Image src="/google.svg" alt="Fazer login com Google" width={18} height={18} />
-                  Google
-                </Button>
+                <SignInDialog />
               </DialogContent>
             </Dialog>
           </>
